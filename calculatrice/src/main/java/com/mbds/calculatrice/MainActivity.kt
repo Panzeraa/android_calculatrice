9069aa65f96bd45aaad4fe3f3498abbde203fc9d
Panz <alexpanz06@gmail.com>
2020-10-20 23:16:21 +0200

    update

--- calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
+++ calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
@@ -2,10 +2,7 @@ package com.mbds.calculatrice
 
 import android.R
 import android.os.Bundle
-import android.text.method.TextKeyListener.clear
 import android.view.LayoutInflater
-import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,77 +11,132 @@ import com.mbds.calculatrice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
-    lateinit var un : TextView
+    lateinit var binding: ActivityMainBinding
+    var resultat: Double = 0.0
+    var operation: String = "+"
+    var nombre = "0"
+    var finCalcul = false
+    var operationTemp = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(baseContext))
         setContentView(binding.root)
-
         initViews()
     }
 
 
-    private fun initViews(){
-        binding.un.setOnClickListener(){
+    private fun initViews() {
+        binding.zero.setOnClickListener() {
+            buttonListener(binding.zero)
+        }
+        binding.un.setOnClickListener() {
             buttonListener(binding.un)
         }
-        binding.deux.setOnClickListener(){
+        binding.deux.setOnClickListener() {
             buttonListener(binding.deux)
         }
-binding.trois.setOnClickListener(){
+        binding.trois.setOnClickListener() {
             buttonListener(binding.trois)
         }
-        binding.quatre.setOnClickListener(){
+        binding.quatre.setOnClickListener() {
             buttonListener(binding.quatre)
         }
-        binding.cinq.setOnClickListener(){
+        binding.cinq.setOnClickListener() {
             buttonListener(binding.cinq)
         }
-        binding.six.setOnClickListener(){
+        binding.six.setOnClickListener() {
             buttonListener(binding.six)
         }
-        binding.sept.setOnClickListener(){
+        binding.sept.setOnClickListener() {
             buttonListener(binding.sept)
         }
-        binding.huit.setOnClickListener(){
+        binding.huit.setOnClickListener() {
             buttonListener(binding.huit)
         }
-        binding.neuf.setOnClickListener(){
+        binding.neuf.setOnClickListener() {
             buttonListener(binding.neuf)
         }
-        binding.divise.setOnClickListener(){
-            buttonListener(binding.divise)
+        binding.multiplie.setOnClickListener() {
+            buttonListener(binding.multiplie)
         }
-        binding.plus.setOnClickListener(){
+        binding.plus.setOnClickListener() {
             buttonListener(binding.plus)
         }
+        binding.divise.setOnClickListener() {
+            buttonListener(binding.divise)
+        }
+        binding.moins.setOnClickListener() {
+            buttonListener(binding.moins)
+        }
+        binding.egal.setOnClickListener() {
+            buttonListenerEgal()
+        }
+    }
 
-
-        //        mailInput = findViewById(R.id.username)
-//        passwordInput = findViewById(R.id.password)
-////        loginButton.isEnabled = true
-//        loginButton.setOnClickListener(){
-//            if(!mailInput.text.isNullOrBlank() && !passwordInput.text.isNullOrBlank()) {
-//                val intent = Intent(baseContext, MainActivity::class.java)
-//
-//                startActivity(intent)
-//            }
-//            else{
-//                Toast.makeText(baseContext, "Erreur", Toast.LENGTH_SHORT).show()
-//
-//            }
-//        }
+    private fun buttonListenerEgal() {
+        ope(operation)
+        binding.expression.append(" = ")
+        finCalcul = true
     }
 
     private fun buttonListener(EditView: TextView) {
-        binding.expression.append(EditView.text)
+        if (finCalcul) {
+            binding.expression.text = ""
+            binding.resultat.text = ""
+            nombre = "0"
+            operation = "+"
+            resultat = 0.0
+            finCalcul = false
+        }
+
+        when (EditView.text) {
+            "+" -> {
+                checkNombre(EditView.text.toString())
+                operation = "+"
+            }
+            "-" -> {
+                checkNombre(EditView.text.toString())
+                operation = "-"
+            }
+            "*" -> {
+                checkNombre(EditView.text.toString())
+                operation = "*"
+            }
+            "/" -> {
+                checkNombre(EditView.text.toString())
+                operation = "/"
+            }
+            else -> {
+                nombre += EditView.text
+                binding.expression.append(EditView.text)
+            }
+        }
+    }
+
+    private fun checkNombre(txt: String){
+        if (nombre != "") {
+            binding.expression.append(txt)
+            ope(operation)
+        } else {
+            binding.expression.text = binding.expression.text.dropLast(1)
+            binding.expression.append(txt)
+        }
+    }
+
+    private fun ope(oper: String) {
+        when (oper) {
+            "+" -> resultat += nombre.toDouble()
+            "-" -> resultat -= nombre.toDouble()
+            "/" -> resultat /= nombre.toDouble()
+            "*" -> resultat *= nombre.toDouble()
+        }
+        nombre = ""
+        binding.resultat.text = resultat.toString()
     }
 
 
     override fun onResume() {
         super.onResume()
-//        loginButton.isEnabled = false
     }
 
 }
